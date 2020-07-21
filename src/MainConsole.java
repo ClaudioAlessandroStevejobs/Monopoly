5f36df14444f34bc6319296b7a4cffe9e3a2029f
ClaudioAlessandroStevejobs <68290416+ClaudioAlessandroStevejobs@users.noreply.github.com>
2020-07-21 17:01:29 +0200

    0.4

--- src/MainConsole.java
+++ src/MainConsole.java
@@ -3,7 +3,14 @@ import sample.Field;
 public class MainConsole {
     public static void main(String[] args) {
         Field f = new Field();
-
+        System.out.println(f);
+        System.out.println(f);
+        System.out.println(f);
+        System.out.println(f);
+        System.out.println(f);
+        System.out.println(f);
+        System.out.println(f);
+        System.out.println(f);
         System.out.println(f);
 
     }
